470bde69dc6bc08b5d6cded6c498f8e4df221340
evlynwn <santiagoevellyn008@gmail.com>
2026-05-28 20:26:52 -0300

    em andamento

--- script.js
+++ script.js
@@ -1,18 +1,92 @@
-const pesquisa = document.getElementById("pesquisa");
+import produtos from "./produtos.json" with { type: "json" }
 
-const cards = document.querySelectorAll(".card");
+const container = document.getElementById("container")
+
+function criarEstrelas(classificacao){
+
+    let estrelas = ""
+
+    for(let i = 1; i <= 5; i++){
+
+        if(i <= classificacao){
+            estrelas += "★"
+        }
+        else{
+            estrelas += "☆"
+        }
+
+    }
+
+    return estrelas
+}
+
+function mostrarProdutos(lista){
+
+    container.innerHTML = ""
+
+    lista.forEach(produto => {
+
+        const card = document.createElement("div")
+
+        card.classList.add("card")
+
+        if(produto.categoria === "Informática"){
+            card.classList.add("informatica")
+        }
+        else{
+            card.classList.add("eletronicos")
+        }
+
+        card.innerHTML = `
+
+        <p class="estrelas">
+        ${criarEstrelas(produto.classificacao)}
+        </p>
+        
+            <img src="./img/${produto.imagem}">
+
+            <h2>${produto.nome}</h2>
+
+            <p class="descricao">
+                ${produto.descricao}
+            </p>
+
+            <p class="preco">
+                R$ ${produto.preco.toFixed(2)}
+            </p>
+
+            <p class="categoria">
+                ${produto.categoria}
+            </p>
+
+            <p class="estrelas">
+                ${criarEstrelas(produto.classificacao)}
+            </p>
+
+            <button>
+                Comprar
+            </button>
+        
+        `
+
+        container.appendChild(card)
+
+    })
+
+}
+
+mostrarProdutos(produtos)
+
+const pesquisa = document.getElementById("pesquisa")
 
 pesquisa.addEventListener("input", () => {
 
-    const valor = pesquisa.value.toLowerCase();
-    cards.forEach(card => {
+    const valor = pesquisa.value.toLowerCase()
 
-        const nome = card.dataset.nome.toLowerCase();
-        if(nome.includes(valor)){
-            card.style.display = "flex";
-        }
-        else{
-            card.style.display = "none";
-        }
-    });
-});
+    const produtosFiltrados = produtos.filter(produto => 
+        produto.nome.toLowerCase().includes(valor)
+    )
+
+    mostrarProdutos(produtosFiltrados)
+
+})
